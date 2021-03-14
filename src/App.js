448934f1c95547f5e1aa936a32787ff096569283
Hilary_not_Hillary <hilarysoong@gmail.com>
2021-03-14 08:48:52 +0800

    KeyCode added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ToDoList from './components/ToDoList';
 import NoteList from './components/NoteList';
 
-function App() {
-  let items = [
-    {
-      id: 1,
-      text:"Learn Javascript",
-      completed: false
-    },
-    { id: 2,
-      text:"Learn ReactJs",
-      completed: false
-    },
-    { id: 3,
-      text:"Build React App",
-      completed: false
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      items : [
+        {
+          id: 1,
+          text:"Learn Javascript",
+          completed: false
+        },
+        { id: 2,
+          text:"Learn ReactJs",
+          completed: false
+        },
+        { id: 3,
+          text:"Build React App",
+          completed: false
+        }
+      ],
+      noteItems: [
+        {
+            id: 1,
+            text:'Note item 1',
+            completed: false
+        },
+        {
+            id: 2,
+            text:'Note item 2',
+            completed: false
+        },
+        {
+            id: 3,
+            text:'Note item 3',
+            completed: false
+        }
+      ] 
     }
-  ];
-  let title = "Things to do";
+  }
+       
+  render(){
+    let title = "Things to do";
 
-  let noteItems = [
-    {
-        id: 1,
-        text:'Note item 1',
-        completed: false
-    },
-    {
-        id: 2,
-        text:'Note item 2',
-        completed: false
-    },
-    {
-        id: 3,
-        text:'Note item 3',
-        completed: false
-    }
-  ];
-
-  let noteTitle = "Note List";
-
+    let noteTitle = "Note List";
     return (
-     <div className="container">
-       <div className="row">
-          <ToDoList title={title} items={items}/>
-          {/* <NoteList noteTitle={noteTitle} noteItems={noteItems}/> */}
-       </div>
-     </div>
-    );
+      <div className="container">
+        <div className="row">
+            <ToDoList title={title} items={this.state.items}/>
+            {/* <NoteList noteTitle={noteTitle} noteItems={noteItems}/> */}
+        </div>
+      </div>
+      );
   
+  };
+      
 }
-
 export default App;
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import * as KeyCode from 'keycode-js';
 
-export default function InputBox(){
-    return(
-        <input type="text" className="form-control add-todo" placeholder="Add New"/>
-    )
+ class InputBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value : props.value || ''
+        }
+    }
+    
+    handleChange(e) {
+        this.setState({value: e.target.value});
+        
+    }
+
+    handleKeyUp(e){
+        if(e.keyCode === KeyCode.KEY_RETURN){
+
+        }
+    }
+    render(){
+        return(
+            <input type="text" className="form-control add-todo" 
+            placeholder="Add New" 
+            value= {this.state.value}
+            onKeyUp= {this.handleKeyUp.bind(this)}
+            onChange={this.handleChange.bind(this)}/>
+        )
+        
+    }
+        
+
 }
+
+export default InputBox;
